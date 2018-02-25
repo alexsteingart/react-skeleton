5f89eb1e89cdcc578dd6b1b0946c3caae704cded
Alex Steingart <alexsteingart@gmail.com>
2018-02-25 15:01:10 -0500

    First commit of skeleton

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context)
+  }
+  render() {
+    return (
+      <div className="App">
+        hello world
+      </div>
+    );
+  }
+}
+
+export default App;
